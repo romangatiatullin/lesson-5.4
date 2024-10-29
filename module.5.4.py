438c86efa3317bc3e3d515961906068bac9533e9
romangatiatullin <mr.artemka02@mail.ru>
2024-10-29 21:26:06 +0300

    Add files via upload

---
+++ module.5.4.py
@@ -0,0 +1,75 @@
+class House:
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        instance = super(House, cls).__new__(cls)
+        cls.houses_history.append(args[0])
+        return instance
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __del__(self):
+        print(f"{self.name} снесён, но он останется в истории")
+
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print('Такого этажа не существует')
+        else:
+            for floor in range(1, new_floor + 1):
+                print(floor)
+
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+
+    def __eq__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        return False
+
+    def __lt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        return False
+
+    def __le__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+        return False
+
+    def __gt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+        return False
+
+    def __ge__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+        return False
+
+    def __ne__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
+        return True
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+        return self
+
+    def __radd__(self, value):
+        return self.__add__(value)
+
+    def __iadd__(self, value):
+        return self.__add__(value)
+
+h1 = House('ЖК Эдем', 15)
+print(House.houses_history)
+h2 = House('ЖК Фили', 30)
+print(House.houses_history)
+h3 = House('ЖК Простоквашино', 5)
+print(House.houses_history)
+del h2
+del h3
+print(House.houses_history)
